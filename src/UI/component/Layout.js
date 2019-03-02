19930bf8a1a9829d7dd89797113f010f690e69e1
edevars <e.devars.dev@gmail.com>
2019-03-02 12:27:18 -0600

    Adding a progress bar

--- src/UI/component/Layout.js
+++ src/UI/component/Layout.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./header";
-import Head from 'next/head'
+import Head from "next/head";
 import GlobalStyle from "../../../style/globalStyle";
+import NProgress from "nprogress";
+import Router from "next/router";
 
-const Layout = (props)=> {
-  
-    return (
-      <>
-        <GlobalStyle />
-        <Head>
-          <title>{props.title}</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </Head>
-        <Header/>
-        {props.children}
-      </>
-    );
-  }
+Router.events.on("routeChangeStart", url => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+const Layout = props => {
+  return (
+    <>
+      <GlobalStyle />
+      <Head>
+        <title>{props.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Header />
+      {props.children}
+    </>
+  );
+};
 
 export default Layout;
